74cc91b3c567eba0c7558c9f79e699dd91d4b1d3
Michal Szestowicki <m.shestee@gmail.com>
2022-03-30 21:07:00 +0200

    Initial commit

---
+++ src/main/java/com/shestee/recruitmenttaskus/RecruitmentTaskUsApplication.java
@@ -0,0 +1,13 @@
+package com.shestee.recruitmenttaskus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentTaskUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentTaskUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shestee/recruitmenttaskus/RecruitmentTaskUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shestee.recruitmenttaskus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentTaskUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
